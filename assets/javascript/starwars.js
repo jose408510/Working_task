4289054c6a6d93f887d0c2e3f8a84095a59379b3
Jose Castellanos <josecastellanos@Joses-MacBook-Pro-2.local>
2018-05-15 18:54:15 -0700

    starting files/ html done/ css done/ only need logic

---
+++ assets/javascript/starwars.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+var characters = {
+    "Obi-Wan Kenobi":{
+        name:"Obi-Wan Kenobi",
+        health: 150,
+        attack: 30,
+        imageUrl: "assets/images/obi-wan.jpg",
+        EnemyAttackBack: 15
+},
+    "Luke skywalker":{
+        name:"Luke Skywalker",
+        health: 200,
+        attack: 50,
+        imageUrl: "assets/images/luke-skywalker.jpg",
+        EnemyAttackBack: 10
+}, 
+    "Darth sidious":{
+        name:"Dark Sidious",
+        health: 70,
+        attack: 25,
+        imageUrl: "assets/images/darth-sidious.png",
+        EnemyAttackBack: 50
+},
+    "Darth Maul":{
+        name:"Darth Maul",
+        health: 100,
+        attack: 10,
+        imageUrl: "assets/images/darth-maul.jpg",
+        EnemyAttackBack: 25
+    }
+}
+
+});
